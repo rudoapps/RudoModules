f653187505d913d0568a87dcc3c856a8fd4f3b80
Toni García Alhambra <toni@rudo.es>
2019-10-25 11:43:07 +0200

    Initial Commit

---
+++ RudoModules/RudoModules.h
@@ -0,0 +1,19 @@
+//
+//  RudoModules.h
+//  RudoModules
+//
+//  Created by Toni García Alhambra on 25/10/2019.
+//  Copyright © 2019 rudo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RudoModules.
+FOUNDATION_EXPORT double RudoModulesVersionNumber;
+
+//! Project version string for RudoModules.
+FOUNDATION_EXPORT const unsigned char RudoModulesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RudoModules/PublicHeader.h>
+
+
